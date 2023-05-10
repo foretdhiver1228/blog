f1611f96bb467b9ec5f51978e060e5c3153a2e78
foretdhiver1228 <zwielichter@gmail.com>
2023-05-10 20:20:58 +0900

    수정했음

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   // state 만드는법, state == 1. 변수 대신 데이터를 저장하는 공간. 2. useState()를 이용해 만들어야 함
   // useState('asdf') == [state데이터, state데이터 변경함수] 형태의 array가 남음
   // 그냥 변수에 담으면 새로고침해야 랜더링되는데.. state는 변경되면 HTML이 자동으로 재 랜더링 됨. 
-  // 중요 데이터는 꼭 state로 데이터 바인딩 시키자!
+  // 중요 데이터는 꼭 state로 데이터 바인딩 시키자! 화이팅!
   let [글제목, 글제목변경] = useState(['즐거운 농구교실', '축구는 어려워', '야구는 어려운 스포츠다']);    // es6 destructuring 문법 let [a,b] = [10, 100];
   let [cnt, cnt변경] = useState(0);
   let [icon, icon변경] = useState('▲');
